da63640cde6484a08fac55c64a2fc298e9969c81
gayatri <gayatri@gayatris-MacBook-Pro.local>
2023-07-13 14:23:36 -0700

    stuff from figma

---
+++ src/components/NavItem/NavItem.stories.js
@@ -0,0 +1,13 @@
+import { NavItem } from ".";
+
+export default {
+  title: "Components/NavItem",
+  component: NavItem,
+};
+
+export const Default = {
+  args: {
+    className: {},
+    navItemClassName: {},
+  },
+};
